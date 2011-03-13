0218808479f4cd4f8dfa3666bd1ad6720c97fd2d
adredz <adredz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-03-13 13:34:34 +0000

    Start version 1.0.3
    git-svn-id: https://plugins.svn.wordpress.org/paypal-subscription-button/tags/1.0.3@359463 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ php/psb_mp_types.php
@@ -0,0 +1,56 @@
+<?php 
+if( !class_exists( 'psb_MP_Types' ) ) {
+	
+	class psb_MP_Types {
+		var $payment_amount;
+		var $payment_type;
+		var $membership_type;
+		
+		function __construct( $payment_amount ) {
+			$this->payment_amount = $payment_amount;
+			$psb_options = get_option( 'psb_admin_options' );
+			$mp_type = array();
+			
+			foreach( $psb_options[ 'payment_amounts' ] as $key => $value ) {
+				//Determines the payment type and membership type of the current transaction
+				if( $this->is_match_amount( $value ) ) {
+					$mp_type = $this->get_mp_type( explode( '_', $key ) );
+					$this->payment_type = $mp_type[ 0 ];
+					$this->membership_type = $mp_type[ 1 ];  	
+				}
+			} 
+		}
+		
+		function get_mp_type( $index ) {
+			//Forms an array out of index var a_monthly_silver.
+			//The result should be something like: array( 'monthly', 'silver' )
+			$mp_type = array();
+			foreach( $index as $type ) {
+				if( $type !== 'a' ) {
+					$mp_type[] = $type;	
+				}
+			}
+			return $mp_type;
+		}
+		
+		function is_match_amount( $amount ) {
+			//Compares the amount from paypal to the ones from settings if there's a match
+			//If so, it returns true
+			if( $amount == $this->payment_amount ) {
+				return true;
+			} else {
+				false;
+			}
+		}
+		
+		function get_membership_type() {
+			//Returns the membership type: silver
+			return $this->membership_type;
+		}
+		
+		function get_payment_type() {
+			//Returns the payment type: monthly
+			return $this->payment_type;
+		}
+	}
+}
