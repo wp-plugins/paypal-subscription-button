4a4d931434577a73c7cba8305ec7945a5552dd21
adredz <adredz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-03 00:13:01 +0000

    Disabled deletion of tables during uninstallation.
    git-svn-id: https://plugins.svn.wordpress.org/paypal-subscription-button/trunk@368263 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/psb_uninstall.php
+++ includes/psb_uninstall.php
@@ -1,19 +1,24 @@
 <?php
 //Uninstalls custom database tables.
 function psb_uninstall() {
-	global $wpdb;
-  	$table_name1 = $wpdb->prefix . "psb_members";
-  	$table_name2 = $wpdb->prefix . "psb_transactions";
-	$table_name3 = $wpdb->prefix . "psb_cancelled";
-  	$wpdb->query("DROP TABLE IF EXISTS $table_name1");
-  	$wpdb->query("DROP TABLE IF EXISTS $table_name2");
-	$wpdb->query("DROP TABLE IF EXISTS $table_name3");
+    global $wpdb;
+    $table_name1 = $wpdb->prefix . "psb_members";
+    $table_name2 = $wpdb->prefix . "psb_transactions";
+    $table_name3 = $wpdb->prefix . "psb_cancelled";
 
-	psb_delete_options();
+    /**
+     * Remove // below to enable deleting of tables during uninstall.
+     */
+        
+    //$wpdb->query("DROP TABLE IF EXISTS $table_name1");
+    //$wpdb->query("DROP TABLE IF EXISTS $table_name2");
+    //$wpdb->query("DROP TABLE IF EXISTS $table_name3");
+
+    psb_delete_options();
 }
 
 function psb_delete_options() {
-	//Add options for admin settings.
+    //Add options for admin settings.
     delete_option("psb_db_version");
-	//delete_option("psb_admin_options");
+    //delete_option("psb_admin_options");
 }
