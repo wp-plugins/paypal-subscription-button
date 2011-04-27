3d9763137b145075034d55591aaddbda71ea683a
adredz <adredz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-27 06:09:00 +0000

    Copy files from 1.2.0 to 1.2.1
    git-svn-id: https://plugins.svn.wordpress.org/paypal-subscription-button/tags/1.2.1@377737 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/psb_uninstall.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Uninstall untility
+ */
+
+function psb_uninstall()
+{
+    global $wpdb;
+    $table_name1 = $wpdb->prefix . "psb_members";
+    $table_name2 = $wpdb->prefix . "psb_transactions";
+    $table_name3 = $wpdb->prefix . "psb_cancelled";
+
+    /**
+     * Remove // below to enable deleting of tables during uninstall.
+     */
+        
+    //$wpdb->query("DROP TABLE IF EXISTS $table_name1");
+    //$wpdb->query("DROP TABLE IF EXISTS $table_name2");
+    //$wpdb->query("DROP TABLE IF EXISTS $table_name3");
+
+    psb_delete_options();
+}
+
+function psb_delete_options()
+{
+    //Add options for admin settings.
+    delete_option("psb_db_version");
+    //delete_option("psb_admin_options");
+}
