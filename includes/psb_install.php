c5969caf03f4d0ca1e3668567ce69c5f6dda0dfe
adredz <adredz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-10 10:14:49 +0000

    Make upgrading smarter by detecting compatibility
    git-svn-id: https://plugins.svn.wordpress.org/paypal-subscription-button/tags/1.2.2-alpha@395414 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/psb_install.php
+++ includes/psb_install.php
@@ -8,14 +8,15 @@ function psb_install ()
 {
    global $wpdb;
    
+   $new_version = '1.2.2';
+   $current_version = get_option('psb_version');
+   $version_flag = 0;
+   
    $table_name1 = $wpdb->prefix . "psb_members";
    $table_name2 = $wpdb->prefix . "psb_transactions";
    $table_name3 = $wpdb->prefix . "psb_cancelled";
-   if ( $wpdb->get_var("show tables like '$table_name1'") != $table_name1
-        AND $wpdb->get_var("show tables like '$table_name2'") != $table_name2
-        AND $wpdb->get_var("show tables like '$table_name3'") != $table_name3 )
-   {
-      $sql = "CREATE TABLE " . $table_name1 . " (
+   
+   $sql = "CREATE TABLE " . $table_name1 . " (
 	  member_id int(20) unsigned NOT NULL auto_increment,
 	  wp_user_id int(20) unsigned NOT NULL,
 	  membership_type varchar(32) NOT NULL,
@@ -25,7 +26,7 @@ function psb_install ()
 	  PRIMARY KEY  (member_id)
 	) $charset_collate;";  
 	
-	  $sql .= "CREATE TABLE " . $table_name2 . " (
+   $sql .= "CREATE TABLE " . $table_name2 . " (
 	  trans_id int(20) unsigned NOT NULL auto_increment,
 	  wp_user_id int(20) unsigned NOT NULL,
 	  date datetime NOT NULL,
@@ -34,39 +35,53 @@ function psb_install ()
 	  first_name varchar(50) NOT NULL,
 	  last_name varchar(50) NOT NULL,
 	  payer_email varchar(75) NOT NULL,
+          ipn longtext NULL,
 	  PRIMARY KEY  (trans_id),
 	  UNIQUE KEY transact (txn_id)
 	) $charset_collate;"; 	
 	
-	  $sql .= "CREATE TABLE " . $table_name3 . " (
+   $sql .= "CREATE TABLE " . $table_name3 . " (
 	  cancel_id int(20) unsigned NOT NULL auto_increment,
 	  wp_user_id int(20) unsigned NOT NULL,
 	  date datetime NOT NULL,
 	  PRIMARY KEY  (cancel_id)
 	) $charset_collate;";
-
-       if (get_option('psb_db_version') !== '1.2.2')
+   
+   if ( $wpdb->get_var("show tables like '$table_name1'") != $table_name1
+        AND $wpdb->get_var("show tables like '$table_name2'") != $table_name2
+        AND $wpdb->get_var("show tables like '$table_name3'") != $table_name3 )
+   {
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
+   }
+   else
+   {
+       if ($current_version !== $new_version OR empty($current_version))
        {
           /*
            * If the db is old which means the plugin is an older version, delete subscr_id columns since they're not needed anymore.
            */
           
-          $sql .= "ALTER TABLE". $table_name2 ."ADD ipn LONGTEXT(1024) NULL AFTER payer_email";
-           
-          $sql .= "ALTER TABLE". $table_name1 ."DROP COLUMN subscr_id;";
-          $sql .= "ALTER TABLE". $table_name2 ."DROP COLUMN subscr_id;";
-          $sql .= "ALTER TABLE". $table_name3 ."DROP COLUMN subscr_id;";
+          $sql .= "ALTER TABLE". $table_name2 ."ADD ipn LONGTEXT NULL";
+          
+          if ($current_version == '1.2.0')
+          {
+              $sql .= "ALTER TABLE". $table_name1 ."DROP COLUMN subscr_id;";
+              $sql .= "ALTER TABLE". $table_name2 ."DROP COLUMN subscr_id;";
+              $sql .= "ALTER TABLE". $table_name3 ."DROP COLUMN subscr_id;";
+          }
+          
+          update_option('psb_version', $new_version);
+          
+          $version_flag = 1;  
+          
+          require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+          dbDelta($sql);
        }
-
-      require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-      dbDelta($sql);
    }
 
-   psb_add_options();
-}
-
-function psb_add_options()
-{
-    //Add options for admin settings.
-    update_option('psb_db_version', '1.2.2');
+   if (!$version_flag)
+   {
+        update_option('psb_version', $new_version);
+   }
 }
