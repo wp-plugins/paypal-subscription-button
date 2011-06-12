26897ac1d197228108783d3622a13c2268d6f04c
adredz <adredz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-06-12 19:08:44 +0000

    Remove deletion of options
    git-svn-id: https://plugins.svn.wordpress.org/paypal-subscription-button/tags/1.2.3-alpha@396330 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/psb_uninstall.php
+++ includes/psb_uninstall.php
@@ -8,13 +8,15 @@ function psb_uninstall()
     global $wpdb;
     $table_name1 = $wpdb->prefix . "psb_members";
     $table_name2 = $wpdb->prefix . "psb_transactions";
-    $table_name3 = $wpdb->prefix . "psb_cancelled";
+    $table_name3 = $wpdb->prefix . "psb_ended";
+    $table_old_name3 = $wpdb->prefix . "psb_cancelled";
 
     /**
-     * Remove // below to enable deleting of tables during uninstall.
+     * Remove comments below to enable deleting of tables during uninstall.
      */
         
     //$wpdb->query("DROP TABLE IF EXISTS $table_name1");
     //$wpdb->query("DROP TABLE IF EXISTS $table_name2");
     //$wpdb->query("DROP TABLE IF EXISTS $table_name3");
+    //$wpdb->query("DROP TABLE IF EXISTS $table_old_name3");
 }
